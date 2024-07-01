7d6ea49b5e18430e29767b9d6218ae1a331537d1
vedze <ezdev00@gmail.com>
2024-07-01 10:24:46 +0900

    Fix: Homepage 디자인 수정, 캐치프레이즈 constants로 수정

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -3,4 +3,6 @@ export const LAST_UPDATED = "2024.07.01";
 export const PHONE_NUM = "010-0000-0000";
 export const EMAIL_ADD = "00@naver.com";
 
+export const CATCH_PHRASE = "이상을 꿈꾸는 개발자";
+
 export const DESKTOP_CONTENT_WIDTH = "900px";
