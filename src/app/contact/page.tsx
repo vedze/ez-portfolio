a966178731a361c4764b4bda2b688b3fa00cc170
vedze <ezdev00@gmail.com>
2024-07-01 16:33:39 +0900

    Fix: 각 페이지 CommonFrame 적용

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,20 +1,11 @@
-import styles from "@/styles/contact.module.css";
-import Section from "@/components/Section";
-import Input from "@/components/Input";
+import CommonFrame from "@/components/CommonFrame";
+import ContactContent from "./ContactContent";
 
 export default function Contact() {
   return (
-    <div className={styles.contactContainer}>
-      <Section name="Contact & Feedback" />
-      <div className={styles.contactText}>
-        <p>
-          여기까지 읽어주셔서 감사합니다!
-          <br />
-          짧은 피드백 한 번 남겨주시면 감사하겠습니다!
-        </p>
-      </div>
-      <Input />
-      {/* 이메일 공간 */}
-    </div>
+    <CommonFrame
+      sectionName="Contact & Feedback"
+      content={<ContactContent />}
+    />
   );
 }
--- src/app/introduce/page.tsx
+++ src/app/introduce/page.tsx
@@ -1,16 +1,8 @@
-import styles from "@/styles/introduce.module.css";
-import Section from "@/components/Section";
+import CommonFrame from "@/components/CommonFrame";
+import IntroduceContent from "./IntroduceContent";
 
 export default function Introduce() {
   return (
-    <div className={styles.introContainer}>
-      <Section name="Introduction" />
-      <p>introduction 페이지 설명입니다.</p>
-      <p>introduction 페이지 설명입니다.</p>
-      <p>introduction 페이지 설명입니다.</p>
-      <p>introduction 페이지 설명입니다.</p>
-      <br />
-      <p>introduction 페이지 설명입니다.</p>
-    </div>
+    <CommonFrame sectionName="Introduction" content={<IntroduceContent />} />
   );
 }
