a5f321e2c5f5e10ed7456602b29c6cbc121fdb64
vedze <ezdev00@gmail.com>
2024-07-01 16:32:02 +0900

    Feat: 페이지 공통 컴포넌트 CommonFrame 구현

---
+++ src/components/CommonFrame.tsx
@@ -0,0 +1,16 @@
+import styles from "@/styles/commonframe.module.css";
+import Section from "./Section";
+
+interface FrameProps {
+  sectionName: String;
+  content?: React.ReactNode;
+}
+
+export default function CommonFrame({ sectionName, content }: FrameProps) {
+  return (
+    <div className={styles.frameContainer}>
+      <Section name={sectionName} />
+      {content ? content : "아직 content가 없습니다."}
+    </div>
+  );
+}
