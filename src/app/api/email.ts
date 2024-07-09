716cfb1db00a55b9a9b3d93e3dc1dcbe28483ad5
vedze <ezdev00@gmail.com>
2024-07-09 16:19:52 +0900

    Feat: nodemailer로 contact 이메일 받기

--- src/app/api/email.ts
+++ src/app/api/email.ts
@@ -2,44 +2,66 @@
 
 import nodemailer from "nodemailer";
 
-export type ContactFormType = {
+export interface ISendContactFormType {
   from: string; // 작성자
   subject: string; // 제목
   text: string; // 내용
-};
+}
 
-type ContactEmailFormType = {
+interface IReceiveContactFormType {
   from: string; // 작성자
   to: string; // 내 메일
   subject: string; // 제목
   html: string; // 이메일 htlm body
-};
+}
 
-export function contactForm({ from, subject, text }: ContactFormType) {
-  // transporter
+export function contactForm({ from, subject, text }: ISendContactFormType) {
+  // naver mail transporter
   const transporter = nodemailer.createTransport({
-    service: "naver",
     host: "smtp.naver.com",
     port: 465,
     secure: true,
     auth: {
-      user: process.env.MY_EMAIL,
-      pass: process.env.MY_EMAIL_PWD,
+      user: process.env.NAVER_EMAIL,
+      pass: process.env.NAVER_PWD,
     },
+    // logger: true,
   });
 
+  // gmail transporter -> ok
+  // const transporter = nodemailer.createTransport({
+  //   host: "smtp.gmail.com",
+  //   port: 465,
+  //   secure: true,
+  //   auth: {
+  //     user: process.env.GOOGLE_EMAIL,
+  //     pass: process.env.GOOGLE_PWD,
+  //   },
+  //   tls: {
+  //     rejectUnauthorized: false,
+  //   },
+  // //   logger: true,
+  // });
+
   // mailing option
-  const contactEmailForm: ContactEmailFormType = {
-    from,
-    to: process.env.MY_EMAIL || "",
-    subject: `test ${subject}`,
+  const contactEmail: IReceiveContactFormType = {
+    from: process.env.NAVER_EMAIL || "",
+    to: process.env.NAVER_EMAIL || "",
+    subject: `[CONTACT] ${subject}`,
     html: `
-        <h1>${subject}</h1>
         <div>${text}</div>
         <br/>
-        <p>보낸 사람: ${from}</p>
+        <strong>보낸 사람: ${from}</strong>
     `,
   };
 
-  return transporter.sendMail(contactEmailForm);
+  // console.log("**contact Email options: ", contactEmail);
+
+  return transporter.sendMail(contactEmail, (error, info) => {
+    if (error) {
+      console.error("오류 발생: ", error);
+    } else {
+      console.log("이메일 전송 성공: ", info.response);
+    }
+  });
 }
--- src/app/api/send-email.ts
+++ src/app/api/send-email.ts
@@ -1,8 +1,10 @@
 // 클라이언트에서 작성한 contactEmailForm 받아와 서버로 api 요청 로직
 
-import { ContactFormType } from "./email";
+import { ISendContactFormType } from "./email";
 
-export async function sendContactEmail(contactEmail: ContactFormType) {
+export async function sendContactEmail(contactEmail: ISendContactFormType) {
+  // console.log("conatctEmail: ", contactEmail); -> ok
+
   const response = await fetch("/api/contact", {
     method: "POST",
     body: JSON.stringify(contactEmail),
@@ -11,7 +13,9 @@ export async function sendContactEmail(contactEmail: ContactFormType) {
     },
   });
 
+  // console.log("response:", response.json());
   const data = await response.json();
+  // console.log("data: ", data);
 
   if (!response.ok) {
     throw new Error(data.message || "서버 요청 실패");
